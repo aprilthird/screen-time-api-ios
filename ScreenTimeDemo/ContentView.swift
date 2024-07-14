b0bf47197e16dd788de5eb39104f0923192c5829
aprilthird <jeffreyrm96@gmail.com>
2024-07-14 01:14:32 -0400

    Initial Commit

---
+++ ScreenTimeDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenTimeDemo
+//
+//  Created by user250962 on 7/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenTimeDemo/ScreenTimeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeDemoApp.swift
+//  ScreenTimeDemo
+//
+//  Created by user250962 on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
