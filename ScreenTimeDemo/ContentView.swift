fa867c455ed87993989cd77bb86441ef04516b5b
aprilthird <jeffreyrm96@gmail.com>
2024-07-24 09:18:01 -0400

    feat: initial

--- ScreenTimeDemo/ContentView.swift
+++ ScreenTimeDemo/ContentView.swift
@@ -6,16 +6,48 @@
 //
 
 import SwiftUI
+import FamilyControls
 
 struct ContentView: View {
+    @State var selection = FamilyActivitySelection()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            
+            FamilyActivityPicker(selection: $selection)
+            
             Text("Hello, world!")
         }
-        .padding()
+        .onAppear() {
+            print("appear")
+            Task {
+                print("task")
+                do {
+                    print("try revoke")
+                    AuthorizationCenter.shared.revokeAuthorization { _ in
+                        Task {
+                            print("task")
+                            do {
+                                print("try authorization")
+                                try await AuthorizationCenter.shared.requestAuthorization(for: .individual)
+                            } catch {
+                                print("error requesting permission")
+                            }
+                        }
+                    }
+                } catch {
+                    print("nothing")
+                }
+            }
+        }
+        .onChange(of: selection) { newSelection in
+            let applications = selection.applications
+            let categories = selection.categories
+            let webDomains = selection.webDomains
+        }
     }
 }
 
